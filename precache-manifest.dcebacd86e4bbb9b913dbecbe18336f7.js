bc9068203200cc4bd75f187183938ce16f84e8d7
Manish Kumar <manishkumar@Manishs-MacBook-Air.local>
2020-02-13 14:59:49 +0530

    Added build package

---
+++ precache-manifest.dcebacd86e4bbb9b913dbecbe18336f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f63eac564f4919d5f5de6bb89f97cb0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3bdfd071c43f79bc1c7",
+    "url": "/static/css/main.0084b761.chunk.css"
+  },
+  {
+    "revision": "a8aa238773620c2384c4",
+    "url": "/static/js/2.cb0ce7bf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.cb0ce7bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3bdfd071c43f79bc1c7",
+    "url": "/static/js/main.7cb73e4a.chunk.js"
+  },
+  {
+    "revision": "3713cc266040d8a04561",
+    "url": "/static/js/runtime-main.f1eb3907.js"
+  }
+]);
